492f9692510b07c8fa5f3e98e33a84e07d5696b2
Ubuntu <ubuntu@ip-172-31-20-208.us-east-2.compute.internal>
2021-10-20 08:11:45 +0000

    Adding customers sql script to models; Removing examples directory

---
+++ models/customers.sql
@@ -0,0 +1,25 @@
+with
+ first_order as (
+     select
+         customer_id,
+         min(created_at) as first_order_at
+     from `analytics-engineers-club.coffee_shop.orders`
+     group by 1
+ ),
+ total_orders as (
+     select
+         customer_id,
+         count(*) as number_of_orders
+     from `analytics-engineers-club.coffee_shop.orders`
+     group by 1
+ )
+ select
+     id as customer_id,
+     name,
+     email,
+     first_order_at,
+     number_of_orders
+ from `analytics-engineers-club.coffee_shop.customers` c
+ left join first_order on c.id = first_order.customer_id
+ left join total_orders on c.id = total_orders.customer_id
+ order by first_order_at 
