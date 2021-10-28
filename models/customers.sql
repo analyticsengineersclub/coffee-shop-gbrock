f301ebf412b4132889a0aead158be7df0fab6e96
Ubuntu <ubuntu@ip-172-31-20-208.us-east-2.compute.internal>
2021-10-28 08:27:29 +0000

    Creating sources and adding tests to dbt project

--- models/customers.sql
+++ models/customers.sql
@@ -7,14 +7,14 @@ with
      select
          customer_id,
          min(created_at) as first_order_at
-     from `analytics-engineers-club.coffee_shop.orders`
+     from {{ source('coffee_shop', 'orders') }} as orders
      group by 1
  ),
  total_orders as (
      select
          customer_id,
          count(*) as number_of_orders
-     from `analytics-engineers-club.coffee_shop.orders`
+     from {{ source('coffee_shop', 'orders') }} as orders
      group by 1
  )
  select
@@ -23,7 +23,7 @@ with
      email,
      first_order_at,
      number_of_orders
- from `analytics-engineers-club.coffee_shop.customers` c
+ from {{ source('coffee_shop', 'customers') }} as c
  left join first_order on c.id = first_order.customer_id
  left join total_orders on c.id = total_orders.customer_id
  order by first_order_at 
--- models/monthly_customers.sql
+++ models/monthly_customers.sql
@@ -1,6 +1,6 @@
 select 
     date_trunc(first_order_at, month) as month_of,
     count(distinct customer_id) as monthly_customers
-from {{ ref('customers') }}` 
+from {{ ref('customers') }} 
 group by 1
 order by 1
